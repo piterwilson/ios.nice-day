171009380aceb3d63ba5d0fc57a0a2643dbcac39
Frank van Boheemen <frankvb@gmail.com>
2019-09-03 14:01:00 +0200

    Adds method for qualifying fetched weather based on (temporary) hardcoded preference

--- ios.nice-day/Modules/Main/MainInteractor.swift
+++ ios.nice-day/Modules/Main/MainInteractor.swift
@@ -19,8 +19,68 @@ class MainInteractor {
     init() {
         openWeatherService = OpenWeatherService()
         openWeatherService?.weatherForCoordinates() { (response, error) in
-            print("\(response)")
-            print("\(error)")
+            guard let weather = response else {
+                print("\(error)")
+                return
+            }
+//            print("\(response)")
+            self.qualify(weather: weather)
         }
+    }
+    
+    //Temporary hardcoded preference
+    let preferredMinTemp: Double = 18
+    let preferredMaxTemp: Double = 27
+    let preferredMaxHumidity: Double = 0.8
+    let preferredMaxRainfall: Double = 0
+    let preferredMaxWindSpeed: Double = 8
+    let preferredMaxCloudiness: Double = 0.2
+    
+    func qualify(weather: Weather) {
+        var weatherRating = 10
+        
+        weatherRating -= determinePenalty(weatherCharacteristic: weather.temp, preference: preferredMinTemp, preferenceType: .minValue)
+        weatherRating -= determinePenalty(weatherCharacteristic: weather.temp, preference: preferredMaxTemp, preferenceType: .maxValue)
+        weatherRating -= determinePenalty(weatherCharacteristic: weather.humidity, preference: preferredMaxHumidity, preferenceType: .maxValue)
+        if let rain = weather.rain {
+            weatherRating -= determinePenalty(weatherCharacteristic: rain, preference: preferredMaxRainfall, preferenceType: .maxValue)
+        }
+        weatherRating -= determinePenalty(weatherCharacteristic: weather.wind.speed, preference: preferredMaxWindSpeed, preferenceType: .maxValue)
+        if let cloudiness = weather.cloudiness {
+            weatherRating -= determinePenalty(weatherCharacteristic: cloudiness, preference: preferredMaxCloudiness, preferenceType: .maxValue)
+        }
+        
+        print("weatherRating: \(weatherRating)")
+    }
+    
+    //Penalties are determined by how much the parameter deviates from the preferred parameter and is capped off on a maximum of 2
+    private func determinePenalty(weatherCharacteristic: Double, preference: Double, preferenceType: PreferenceType) -> Int {
+        let penalty: Int
+        switch preferenceType {
+        case .minValue:
+            if weatherCharacteristic < preference {
+                if preference == 0 {
+                    penalty = floor(weatherCharacteristic / 10) < 2 ? Int(weatherCharacteristic / 10) : 2
+                } else {
+                    let deviation : Double = (preference - weatherCharacteristic) / preference
+                    penalty = ceil(deviation * 10) < 2 ? Int(ceil(deviation * 10)) : 2
+                }
+            } else {
+                penalty = 0
+            }
+        case .maxValue:
+            if weatherCharacteristic > preference {
+                if preference == 0 {
+                    penalty = floor(weatherCharacteristic / 10) < 2 ? Int(weatherCharacteristic / 10) : 2
+                } else {
+                    let deviation : Double = Double((weatherCharacteristic - preference) / preference)
+                    penalty = ceil(deviation * 10) < 2 ? Int(ceil(deviation * 10)) : 2
+                }
+            } else {
+                penalty = 0
+            }
+        }
+        
+        return penalty
     }
 }
