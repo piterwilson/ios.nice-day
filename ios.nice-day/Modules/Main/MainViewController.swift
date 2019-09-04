41273f107850bc3980ea3a3c5e2d77b2b22c0715
Frank van Boheemen <frankvb@gmail.com>
2019-09-04 10:48:03 +0200

    Hooks up UI to view controller using IBOutlets

--- ios.nice-day/Modules/Main/MainViewController.swift
+++ ios.nice-day/Modules/Main/MainViewController.swift
@@ -13,6 +13,46 @@ protocol MainViewControllerDelegate {
 }
 
 class MainViewController: UIViewController {
+    @IBOutlet weak var HeaderLabel: UILabel!
+    @IBOutlet weak var refreshButton: UIButton!
+    
+    @IBOutlet weak var currentTempImageView: UIImageView!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    
+    @IBOutlet weak var currentHumidityImageView: UIImageView!
+    @IBOutlet weak var currentHumidityLabel: UILabel!
+    
+    @IBOutlet weak var currentRainfallImageView: UIImageView!
+    @IBOutlet weak var currentRainfallLabel: UILabel!
+    
+    @IBOutlet weak var currentWindSpeedImageView: UIImageView!
+    @IBOutlet weak var currentWindSpeedLabel: UILabel!
+    
+    @IBOutlet weak var currentCloudinessImageView: UIImageView!
+    @IBOutlet weak var currentCloudinessLabel: UILabel!
+    
+    @IBOutlet weak var preferencesHeaderLabel: UILabel!
+    
+    @IBOutlet weak var tempHeaderLabel: UILabel!
+    @IBOutlet weak var tempSlider: UISlider!
+    @IBOutlet weak var tempValueLabel: UILabel!
+    
+    @IBOutlet weak var humidityHeaderLabel: UILabel!
+    @IBOutlet weak var humiditySlider: UISlider!
+    @IBOutlet weak var humidityValueLabel: UILabel!
+    
+    @IBOutlet weak var rainfallHeaderLabel: UILabel!
+    @IBOutlet weak var rainfallSlider: UISlider!
+    @IBOutlet weak var rainfallValueLabel: UILabel!
+    
+    @IBOutlet weak var windSpeedHeaderLabel: UILabel!
+    @IBOutlet weak var windSpeedSlider: UISlider!
+    @IBOutlet weak var windSpeedValueLabel: UILabel!
+    
+    @IBOutlet weak var cloudinessHeaderLabel: UILabel!
+    @IBOutlet weak var cloudinessSlider: UISlider!
+    @IBOutlet weak var cloudinessValueLabel: UILabel!
+    
     var delegate: MainViewControllerDelegate?
     
     override func viewDidLoad() {
@@ -20,16 +60,4 @@ class MainViewController: UIViewController {
         delegate?.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
